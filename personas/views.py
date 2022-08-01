24481acaf61d276e4f52dd42c6da9d24786f4a78
Anselminie <anselminie@gmail.com>
2022-08-01 16:57:33 -0300

    cambio en fecha de nacimiento en la creacion de datos

--- personas/views.py
+++ personas/views.py
@@ -3,7 +3,7 @@ from personas.models import Persona
 # Create your views here.
 
 def create_persona(request):
-    new_persona = Persona.objects.create(nombre = 'Nancy Susana', apellido = 'Pellegrino', edad = 61, fecha_nacimiento = '1961-03-24')
+    new_persona = Persona.objects.create(nombre = 'Nancy Susana', apellido = 'Pellegrino', edad = 64, fecha_nacimiento = '1958-03-24')
     context = {
         'new_persona' : new_persona
     }
